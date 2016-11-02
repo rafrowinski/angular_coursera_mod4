ab4efe0118ecd5bb1f74d9d65922b3ba02f6d175
szajba <johnny.knetwalker@gmail.com>
2016-11-02 18:48:09 +0100

    first attempt to load images. adding favicon.ico

--- services/flickrimage.service.js
+++ services/flickrimage.service.js
@@ -2,7 +2,7 @@
     'use strict';
     angular.module('data').service('ImageService', ImageService)
         //There is no point in putting constants in separate files
-        .constant('flickImageAPIUrl', 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?');
+        .constant('flickImageAPIUrl', 'https://api.flickr.com/services/feeds/photos_public.gne?nojsoncallback=1?');
 
     ImageService.$inject = ['$http', 'flickImageAPIUrl'];
 
@@ -14,7 +14,7 @@
                 method: 'GET',
                 url: (flickImageAPIUrl),
                 params: {
-                    tags: query.replace(/[^a-zA-Z ]/g, "").split('_').join(' '),
+                    tags: query.replace(/[^a-zA-Z_]/g, "").trim().split('_').join(' '),
                     tagmode: "all",
                     safe_search: 1,
                     content_type: 1,
@@ -22,7 +22,10 @@
                     format: "json"
                 }
             }).then(function(result) {
-                  return result.data.items[0]['media']['m'];
+                return result.data.items[0]['media']['m'];
+            }).error(function(data, status) {
+                console.log(data || "Request failed");
+                console.log(status || "No status");
             });
         };
     };
